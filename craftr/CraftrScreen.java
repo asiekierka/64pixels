ede307abd230a23804c1a8c5e5f4fc310afaea79
Ben Russell (300178622) <ben@95lx.(none)>
2011-02-28 15:48:07 +1300

    eine kleine lightkleanup (a little bit of light cleanup)

---
+++ craftr/CraftrScreen.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.image.*;
+
+public abstract class CraftrScreen
+{
+	public CraftrCanvas c;
+	public CraftrScreen(CraftrCanvas cc)
+	{
+		c = cc;
+	}
+	public CraftrScreen()
+	{
+	
+	}
+	public abstract void paint(Graphics g, int mmx, int mmy);
+}
