957f0e45f2089353b4ea7298fc096e28cdd17b24
asiekierka <asiekierka@gmail.com>
2011-04-22 08:40:24 +0200

    debug code removal

--- client/CraftrNet.java
+++ client/CraftrNet.java
@@ -544,7 +544,6 @@ public class CraftrNet implements Runnable, CraftrNetShim
 								byte t3 = in.readByte();
 								byte ch1 = in.readByte();
 								byte co1 = in.readByte();
-								System.out.println("Received block packet at " + bx1 + "," + by1 + ". " + t3 + "," + ch1 + "," + co1);
  								if(t3 == -1)
  								{
  									synchronized(game.map)
@@ -622,7 +621,6 @@ public class CraftrNet implements Runnable, CraftrNetShim
  								}
 								break;
 							case 0xE1: // push me
-								System.out.println("MULTIPLE PUSH");
 								int e1x = in.readInt();
 								int e1y = in.readInt();
 								int e1xs = in.readShort();
