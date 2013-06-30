c30ffc0f49e2140cd899a2f14ba177038810c2ef
asiekierka <asiekierka@gmail.com>
2013-07-01 01:42:43 +0200

    fix a very severe case of packet spam

--- common/CraftrMapGenerator.java
+++ common/CraftrMapGenerator.java
@@ -1,5 +1,5 @@
 package common;
 
 public abstract class CraftrMapGenerator {
-	public boolean generate(CraftrMap map, int width, int height);
+	public abstract boolean generate(CraftrMap map, int width, int height);
 }
