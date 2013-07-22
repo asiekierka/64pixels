ce983c2a42b8bba2322c3ce69ea04fbc036cbdea
asiekierka <asiekierka@gmail.com>
2013-07-22 13:16:10 +0200

    [REGIONS REWRITE] Add rectangles.

---
+++ common/Rectangle.java
@@ -0,0 +1,28 @@
+package common;
+
+public class Rectangle {
+  int x,y,w,h,xm,ym;
+  public Rectangle(int _x, int _y, int _w, int _h) {
+    x = _x;
+    y = _y;
+    w = _w;
+    h = _h;
+    if(w<0) { // fix width and height
+      w = 0 - w;
+      x -= w;
+    }
+    if(h<0) {
+      h = 0 - h;
+      y -= h;
+    }
+    xm = x+w; // calculate maxes to speed up optimizations
+    ym = y+h;
+  }
+  public int getX() { return x; }
+  public int getY() { return y; }
+  public int getW() { return w; }
+  public int getH() { return h; }
+  public boolean insideRect(int px, int py) {
+    return ((px>=x) && (px<xm) && (py>=y) && (py<ym));
+  }
+}
